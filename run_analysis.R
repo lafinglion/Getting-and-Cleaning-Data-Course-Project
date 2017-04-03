9fe49d5ee4265bc7e639e3a84ecf49eecdf8f3ce
lafinglion <lafinglion@yahoo.com>
2017-04-03 15:29:03 -0700

    Add files via upload

--- run_analysis.R
+++ run_analysis.R
@@ -3,7 +3,7 @@ library(dplyr)
 
 # Download and unzip the necessary data
 fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
-download.file(fileUrl, destfile = "dataset.zip", method="curl")
+download.file(fileUrl, destfile = "dataset.zip")
 if(!file.exists("UCI HAR Dataset")) {unzip("dataset.zip")}
 
 # Read the various data sets
@@ -30,16 +30,13 @@ xMerge <- xMerge[,selected[,1]]
 # Uses descriptive activity names to name the activities in the data set
 colnames(yMerge) <- "activity"
 yMerge$activitylabel <- factor(yMerge$activity, labels = as.character(activity_labels[,2]))
-activitylabel <- yMerge[,2]
+activity <- yMerge[,2]
 
 # Appropriately labels the data set with descriptive variable names.
 colnames(xMerge) <- features[selected[,1],2]
 colnames(subjectMerge) <- "subject"
 
 # From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
-tidyData <- cbind(xMerge, activitylabel, subjectMerge)
-tidyData_mean <- tidyData %>% group_by(activitylabel, subject) %>% summarize_each(funs(mean))
+tidyData <- cbind(xMerge, activity, subjectMerge)
+tidyData_mean <- tidyData %>% group_by(subject, activity) %>% summarize_each(funs(mean))
 write.table(tidyData_mean, file = "UCI HAR Dataset/tidydata.txt", row.names = FALSE, col.names = TRUE)
-
-
-
